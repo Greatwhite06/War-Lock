c8fba4ffb328769320a674bf2384f428caef3b14
Greatwhite06 <jgalvanfims@gmail.com>
2021-03-27 13:33:34 -0500

    Adding main menu screen

---
+++ core/src/brocode/warlock/MainMenu.java
@@ -0,0 +1,45 @@
+package brocode.warlock;
+
+import com.badlogic.gdx.Screen;
+
+public class MainMenu implements Screen {
+
+    public MainMenu(WarLock warLock) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
