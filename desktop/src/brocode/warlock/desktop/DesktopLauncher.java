c873e534353ddfb217f812f28e68e357591ed64f
Greatwhite06 <63685338+Greatwhite06@users.noreply.github.com>
2021-03-26 22:24:00 -0500

    Add files via upload

---
+++ desktop/src/brocode/warlock/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package brocode.warlock.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import brocode.warlock.WarLock;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WarLock(), config);
+	}
+}
