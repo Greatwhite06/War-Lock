9767fd33f11f07aba46e5fcfc8e439ef0396ccae
Brandon <brandon_stevenson01@hotmail.com>
2021-03-27 13:50:05 -0500

    This change captures the description of 3 different virus types and seperates the description by lines. This has been tested to print properly in the desktopLauncher.

---
+++ core/src/brocode/warlock/ComputerObj.java
@@ -0,0 +1,11 @@
+package brocode.warlock;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.lang.String;
+
+public interface ComputerObj {
+    List<String> getBody = null;
+}
