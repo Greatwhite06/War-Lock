0a297a944b66abdad9246a114bb68f66e8ff6441
Luke Ryktarsyk <nzp744@my.utsa.edu>
2021-03-27 14:19:49 -0500

    defined screen height and width created hud definition setup camera

--- core/src/brocode/warlock/Screens/OfficeScreen.java
+++ core/src/brocode/warlock/Screens/OfficeScreen.java
@@ -1,4 +1,59 @@
 package brocode.warlock.Screens;
 
-public class OfficeScreen {
+import brocode.warlock.Scenes.Hud;
+import brocode.warlock.WarLock;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class OfficeScreen implements Screen {
+    private WarLock game;
+    private TextureAtlas atlas;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
+    private Hud hud;
+
+    public OfficeScreen(WarLock game){
+        gamecam = new OrthographicCamera();
+
+        //gamePort = new FitViewport()
+        //hud = new Hud(game.batch);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
--- core/src/brocode/warlock/WarLock.java
+++ core/src/brocode/warlock/WarLock.java
@@ -1,22 +1,29 @@
 package brocode.warlock;
 
-import brocode.warlock.Screens.MainMenu;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class WarLock extends Game {
-	SpriteBatch batch;
-	Texture img;
-	Texture img2;
+	public static final int V_WIDTH = 800;
+	public static final int V_HEIGHT = 416;
+
+	//Pixels per meter: use this float (divisional reasons) to scale everything that has pixel size (positional locations, viewports, character models)
+	public static final float PPM = 100;
+
+	public SpriteBatch batch;
+
+	/* NOTE: using AssetManager in a static way can cause issues. Instead, we may want to pass around Assetmanager to
+	the classes that need it. It is used in a static context here to save time */
+	public static AssetManager manager;
 
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
-		img2 = new Texture("gitpfp.jpg");
-		this.setScreen(new MainMenu(this));
+		manager = new AssetManager();
 
 	}
 
@@ -25,14 +32,11 @@ public class WarLock extends Game {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.draw(img2, 100, 100);
 		batch.end();
 	}
 
 	@Override
 	public void dispose() {
 		batch.dispose();
-		img.dispose();
 	}
 }
