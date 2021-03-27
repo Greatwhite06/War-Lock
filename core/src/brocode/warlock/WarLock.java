ba873b94f83fd7360561dc5ceb314cab0607111c
Luke Ryktarsyk <nzp744@my.utsa.edu>
2021-03-27 13:46:54 -0500

    added new packages and class structure

--- core/src/brocode/warlock/WarLock.java
+++ core/src/brocode/warlock/WarLock.java
@@ -8,6 +8,9 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class WarLock extends Game {
+	public static final int V_WIDTH = 400;
+	public static final int V_HEIGHT = 208;
+
 	public SpriteBatch batch;
 	Texture img;
 	Texture img2;
