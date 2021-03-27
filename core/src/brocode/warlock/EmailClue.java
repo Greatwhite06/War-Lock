cf1ef144b1bb139870151f477fa6e21ac59334e9
Greatwhite06 <jgalvanfims@gmail.com>
2021-03-27 16:14:11 -0500

    added comments to email clue

--- core/src/brocode/warlock/EmailClue.java
+++ core/src/brocode/warlock/EmailClue.java
@@ -19,38 +19,38 @@ public class EmailClue {
         emailPath = "core/assets/Emails/Phising";
 
         if(Gdx.files.internal(emailPath).exists()){
-            int scenarioNum = MathUtils.random(1, 3);
-            int[] choice = new int[4];
-            FileHandle email = Gdx.files.internal(emailPath);
-            Scanner input = new Scanner(email.read());
-
+            int scenarioNum = MathUtils.random(1, 3);//choose variant
+            int[] choice = new int[4]; // format lines [variant] [body lines] [reply lines] [next variant]
+            FileHandle email = Gdx.files.internal(emailPath); //create handle for the file
+            Scanner input = new Scanner(email.read()); //read line by line
 
             while(true){
-                for(int i = 0; i < 3; i++){
-                    choice[i] = input.nextInt();
+                for(int i = 0; i < 3; i++){ //read format lines
+                    choice[i] = input.nextInt(); //fill array with the format numbers
                 }
-                if(choice[0] != scenarioNum){
-                    for(int i = 0; i < choice[1];i++){
-                        this.Body.add(input.next());
+                if(choice[0] != scenarioNum){ //if variant is correct go ahead
+                    for(int i = 0; i < choice[1];i++){ //read for lines associated with body
+                        this.Body.add(input.next()); //add lines to a string list for body
                     }
-                    for(int i = 0; i < choice[2];i++){
-                        this.Reply.add(input.next());
+                    for(int i = 0; i < choice[2];i++){ //read for lines associateed with reply
+                        this.Reply.add(input.next()); // add liens to a string list for reply
                     }
-                    break;
+                    break; //end while loop
                 }
-                else{
-                    for(int i = 0 ;i < choice[3];i++){
-                        input.next();
+                else{ // jump to the next variant
+                    for(int i = 0 ;i < choice[3];i++){ //iterates until next variant format numbers start
+                        input.next(); //reads and does not save skipped lines
                     }
                 }
             }
         }
     }
 
+    //Grab the body
     public List<String> getBody() {
         return Body;
     }
-
+    //grab the reply
     public List<String> getReply() {
         return Reply;
     }
