bbe65db6986e1eb56c09270bca3589dc3e8e87a7
Brandon <brandon_stevenson01@hotmail.com>
2021-03-27 14:14:25 -0500

    This change captures the description of 3 different virus types and seperates the description by lines. This has been tested to print properly in the desktopLauncher.

--- core/src/brocode/warlock/WarLock.java
+++ core/src/brocode/warlock/WarLock.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import brocode.warlock.Screens.MainMenu;
 
 public class WarLock extends Game {
 	SpriteBatch batch;
